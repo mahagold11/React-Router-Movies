3fd693596ad7c0beccfc51d2844bd07aeb541490
Mark Goldhardt <markgoldhardt@Marks-iMac.hsd1.ut.comcast.net>
2020-02-18 00:07:58 -0700

    movie links and cards working

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -20,33 +20,35 @@ export const MovieList = props => {
   }, []);
   
   return (
-    <Link to={`/movie/${movies.id}`} className="movie-list">
+    <div to={`/movie/${movies.id}`} className="movie-list">
       {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
-    </Link>
+    </div>
   );
 }
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+    <Link to={`/movie/${movie.id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+    </Link>
   );
 }
 
